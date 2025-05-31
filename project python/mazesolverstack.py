0363a091ef5e5cacb9f867a77d9c6d75938c703b
Mohammad Saleh Akbari <mohamadsaleh.akb84@gmail.com>
2025-05-31 19:29:20 +0330

    Auto commit

--- project python/mazesolverstack.py
+++ project python/mazesolverstack.py
@@ -12,7 +12,10 @@ print(b)
 
 
 
-###########
+
+
+
+#########################################################
 os.system('git add .')
 os.system('git commit -m "Auto commit"')
 os.system('git push')
